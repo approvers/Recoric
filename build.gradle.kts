9ca523ab45193b88955dc7f351e725a89b586180
mirror-kt <kyoutoday@kyou-today.dev>
2020-03-19 05:44:50 +0900

    initialize of SpringBoot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,14 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
     kotlin("jvm") version "1.3.70"
+    kotlin("plugin.spring") version "1.3.70"
+
     id(Danger.ktlintGradle) version Danger.ktlintGradleVersion
+
+    id("org.springframework.boot") version Spring.version
+    id("io.spring.dependency-management") version Spring.managementVersion
 }
 
 group = "io.github.brokenmanager.recoric"
@@ -14,14 +20,24 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
     }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+
+    withType<Test> {
+        useJUnitPlatform()
     }
 }
 
--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -2,3 +2,8 @@ object Danger {
     const val ktlintGradle = "org.jlleitschuh.gradle.ktlint"
     const val ktlintGradleVersion = "9.2.1"
 }
+
+object Spring {
+    const val version = "2.2.5.RELEASE"
+    const val managementVersion = "1.0.9.RELEASE"
+}
